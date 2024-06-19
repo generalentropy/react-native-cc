539e7d65e511bf2cf415034d5b9ab55c8db2e4ad
entropie <entropie@tuta.io>
2024-06-19 18:52:12 +0200

    update

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-pblack text-3xl">Aora !</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="h-full bg-primary">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="h-full w-full items-center justify-center px-4">
+          <Image
+            source={images.logo}
+            className="h-[84px] w-[130px]"
+            resizeMode="contain"
+          ></Image>
+
+          <Image
+            source={images.cards}
+            className="h-[300px] w-full max-w-[380px]"
+            resizeMode="contain"
+          ></Image>
+
+          <View className="relative mt-5">
+            <Text className="text-center font-pbold text-3xl text-white">
+              Discover Endless possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="absolute -bottom-2 -right-6 h-[15px] w-[130px]"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="mt-7 text-center font-pregular text-sm text-gray-100">
+            Where creativity meets innovation : embark on a journey of limitless
+            exploration with Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
