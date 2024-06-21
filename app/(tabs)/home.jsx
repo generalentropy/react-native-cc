7b7f327ab2c88a48a49a4cc99d599fd857e2d930
entropie <entropie@tuta.io>
2024-06-21 19:30:23 +0200

    empty state component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,19 +1,28 @@
-import { View, Text, FlatList, Image } from "react-native";
-import React from "react";
+import { View, Text, FlatList, Image, RefreshControl } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // Re call video -> if any new videos appeard
+    setRefreshing(false);
+  };
+
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="h-full bg-primary">
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <Text className="text-3xl text-white">{item.id}</Text>
         )}
-        keyExtractor={(item) => item.$id}
         ListHeaderComponent={() => (
           <View className="my-6 space-y-6 px-4">
             <View className="mb-6 flex-row items-start justify-between">
@@ -41,6 +50,15 @@ const Home = () => {
             </View>
           </View>
         )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No videos found"
+            subtitle="Be the first one to upload a video ✨"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </SafeAreaView>
   );
