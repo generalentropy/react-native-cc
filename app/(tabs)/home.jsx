8b4a0a646f3339cd1e7bd2992b7373a23823c11e
entropie <entropie@tuta.io>
2024-06-21 17:32:53 +0200

    tendring component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -3,17 +3,17 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
 
 const Home = () => {
   return (
     <SafeAreaView className="bg-primary">
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-        key={(item) => item.id}
-        keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <Text className="text-3xl text-white">{item.id}</Text>
         )}
+        keyExtractor={(item) => item.$id}
         ListHeaderComponent={() => (
           <View className="my-6 space-y-6 px-4">
             <View className="mb-6 flex-row items-start justify-between">
@@ -24,7 +24,7 @@ const Home = () => {
                 <Text className="font-psemibold text-2xl text-white">Eddy</Text>
               </View>
 
-              <View className="mt-1.5 border-2">
+              <View className="mt-1.5">
                 <Image
                   source={images.logoSmall}
                   className="h-10 w-9"
@@ -32,7 +32,13 @@ const Home = () => {
                 />
               </View>
             </View>
-            <SearchInput />
+            <SearchInput placeholder="Search for a video topic" />
+            <View className="w-full pb-8 pt-5">
+              <Text className="mb-3 font-pregular text-lg text-gray-100">
+                Latest Videos
+              </Text>
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+            </View>
           </View>
         )}
       />
